365d3550cea327d56fff04822d01dd84e138a153
lautarodapin <lautarodapin@gmail.com>
2021-03-09 15:55:17 -0300

    añadido nota

--- models/nota.py
+++ models/nota.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.types import Date
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.sql.expression import null
+from sqlalchemy.types import Date, DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 class Nota(Base):
     __tablename__ = "Notas"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(255))
     titulo = Column(String(100), nullable=False, unique=True)
-    password = Column(String(500))
+    cuerpo = Column(Text(5000), nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    mod_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.current_timestamp())
