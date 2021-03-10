e3a787823815328c2a417270ad607700ab55ad0e
lautarodapin <lautarodapin@gmail.com>
2021-03-10 08:46:00 -0300

    Añadidos tags de organiacion

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -53,13 +54,9 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 @app.get("/")
 def main():
     return RedirectResponse(url="/docs/")
-
-
-@app.post('/login', status_code=status.HTTP_200_OK)
+    
+@app.post('/login', status_code=status.HTTP_200_OK, tags=["auth"])
 def login(user: schemas.UserIn, Authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
-    # if user.username != "test" or user.password != "test":
-    #     raise HTTPException(status_code=401,detail="Bad username or password")
-
     _user = db.query(models.User).filter(models.User.username == user.username).first()
     if _user is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"El usuario {user.username} no esta registrado")
@@ -75,7 +72,7 @@ def login(user: schemas.UserIn, Authorize: AuthJWT = Depends(), db: Session = De
     # return {"access_token": access_token}
 
 
-@app.post('/refresh', status_code=status.HTTP_200_OK)
+@app.post('/refresh', status_code=status.HTTP_200_OK, tags=["auth"])
 def refresh(Authorize: AuthJWT = Depends()):
     Authorize.jwt_refresh_token_required()
 
@@ -85,7 +82,7 @@ def refresh(Authorize: AuthJWT = Depends()):
     return {"msg":"The token has been refresh"}
 
 
-@app.delete('/logout', status_code=status.HTTP_200_OK)
+@app.delete('/logout', status_code=status.HTTP_200_OK, tags=["auth"])
 def logout(Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
 
@@ -93,14 +90,14 @@ def logout(Authorize: AuthJWT = Depends()):
     return {"msg":"Successfully logout"}
 
 
-@app.get('/user', response_model=List[schemas.User], status_code=HTTP_200_OK)
+@app.get('/user', response_model=List[schemas.User], status_code=HTTP_200_OK, tags=["users"])
 def user(Authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
     Authorize.jwt_required()
     users = db.query(models.User).all()
     return users
 
 
-@app.post("/user", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+@app.post("/user", response_model=schemas.User, status_code=status.HTTP_201_CREATED, tags=["users"])
 def create_user(user: schemas.UserIn, db: Session = Depends(get_db)):
     data = models.User(**user.dict())
     data.set_password(data.password)
@@ -109,7 +106,7 @@ def create_user(user: schemas.UserIn, db: Session = Depends(get_db)):
     return data
 
 
-@app.delete("/user/{pk}", status_code=status.HTTP_200_OK)
+@app.delete("/user/{pk}", status_code=status.HTTP_200_OK, tags=["users"])
 async def delete_user(pk: int, db: Session = Depends(get_db), Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
     user = db.query(models.User).get(pk)
@@ -119,7 +116,7 @@ async def delete_user(pk: int, db: Session = Depends(get_db), Authorize: AuthJWT
 
 
 
-@app.get("/user/get-current/", response_model=schemas.User, status_code=status.HTTP_200_OK)
+@app.get("/user/get-current/", response_model=schemas.User, status_code=status.HTTP_200_OK, tags=["users"])
 def get_current_user(auth: AuthJWT = Depends(), db: Session = Depends(get_db)):
     auth.jwt_required()
     username: str = auth.get_jwt_subject()
@@ -127,13 +124,13 @@ def get_current_user(auth: AuthJWT = Depends(), db: Session = Depends(get_db)):
     return user
 
 
-@app.get("/records/", response_model=List[schemas.Record], status_code=status.HTTP_200_OK)
+@app.get("/records/", response_model=List[schemas.Record], status_code=status.HTTP_200_OK, tags=["records"])
 def show_records(db: Session = Depends(get_db)):
     records = db.query(models.Record).all()
     return records
 
 
-@app.post("/records/", response_model=schemas.Record, status_code=status.HTTP_201_CREATED)
+@app.post("/records/", response_model=schemas.Record, status_code=status.HTTP_201_CREATED, tags=["records"])
 def post_record(record: schemas.PostRecord, db: Session = Depends(get_db)):
     data = models.Record(**record.dict())
     db.add(data)
@@ -141,13 +138,13 @@ def post_record(record: schemas.PostRecord, db: Session = Depends(get_db)):
     return data
 
 
-@app.get("/records/{pk}/", response_model=schemas.Record, , status_code=status.HTTP_200_OK)
+@app.get("/records/{pk}/", response_model=schemas.Record, status_code=status.HTTP_200_OK, tags=["records"])
 def get_record(pk: int, db: Session = Depends(get_db)):
     record = db.query(models.Record).filter(models.Record.id == pk).first()
     return record
 
 
-@app.delete("/records/{pk}/", status_code=status.HTTP_200_OK)
+@app.delete("/records/{pk}/", status_code=status.HTTP_200_OK, tags=["records"])
 def delete_record(pk: int, db: Session = Depends(get_db)):
     record = db.query(models.Record).get(pk)
     if not record:
@@ -158,12 +155,12 @@ def delete_record(pk: int, db: Session = Depends(get_db)):
 
 
 """ NOTAS """
-@app.get("/notas", response_model=List[schemas.Nota], status_code=status.HTTP_200_OK)
+@app.get("/notas", response_model=List[schemas.Nota], status_code=status.HTTP_200_OK, tags=["notas"])
 def notas(db: Session = Depends(get_db)):
     notas = db.query(models.Nota).all()
     return notas
 
-@app.post("/notas", response_model=schemas.Nota, status_code=status.HTTP_201_CREATED)
+@app.post("/notas", response_model=schemas.Nota, status_code=status.HTTP_201_CREATED, tags=["notas"])
 def create_nota(nota: schemas.NotaIn, db: Session = Depends(get_db), auth: AuthJWT = Depends()):
     auth.jwt_required()
     nota = models.Nota(**nota.dict())
@@ -171,7 +168,7 @@ def create_nota(nota: schemas.NotaIn, db: Session = Depends(get_db), auth: AuthJ
     db.commit()
     return nota
 
-@app.delete("/notas/{pk}", status_code=status.HTTP_200_OK)
+@app.delete("/notas/{pk}", status_code=status.HTTP_200_OK, tags=["notas"])
 def delete_nota(pk: int, db: Session = Depends(get_db), auth: AuthJWT = Depends()):
     auth.jwt_required()
     nota = db.query(models.Nota).get(pk)
